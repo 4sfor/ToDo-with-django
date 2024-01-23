18d50076fa096c11951b53353bb5031c8efc91cd
Aleksandr Safonov <siaalex03@gmail.com>
2024-01-23 20:10:50 +0400

    start project

---
+++ todo/mainapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название')
+    description = models.TextField(verbose_name='описание')
+    done = models.BooleanField(default=False, verbose_name='выполнено')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    deadline = models.DateTimeField(verbose_name='дедлайн')
+    class Meta:
+        verbose_name = 'Задачи'
+        verbose_name_plural = 'Задача'
+        ordering = ['-created_at']
